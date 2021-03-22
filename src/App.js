d9a828ee2ea7520620d67fa348e439a346faaa47
yourUsernameForGithub <youremailhere>
2021-03-22 16:42:00 +0000

    done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,37 @@ function App() {
       <Navbar/>
       <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
 
-      <DataView/>
+      <DataView
+  borough="Staten Island"
+  renterCost="$14,292"
+  ownerCost="$28,752"
+  renterIncome="$37,882"
+  ownerIncome="$94,177"/>
+
+  <DataView
+  borough="Queens"
+  renterCost="$16,812"
+  ownerCost="$29,256"
+  renterIncome="$46,549"
+  ownerIncome="$80,144"/>
+  <DataView
+  borough="Brooklyn"
+  renterCost="$15,144"
+  ownerCost="$31,908"
+   renterIncome="$40,128"
+  ownerIncome="$83,737"/>
+<DataView
+  borough="Manhattan"
+  renterCost="$18,900"
+  ownerCost="$36,252"
+   renterIncome="$62,173"
+  ownerIncome="$142,046"/>
+<DataView
+  borough="Bronx"
+  renterCost="$13,172"
+  ownerCost="$28,548"
+   renterIncome="$29,302"
+  ownerIncome="$76,454"/>
     </div>
   );
 }
